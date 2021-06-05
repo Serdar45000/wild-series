20e5f95747f9be6718e8f3890a488201d47e9880
Serdar45000 <serdar45000@gmail.com>
2021-06-05 22:36:52 +0200

    17 - Symfony : Les fixtures avancées avec jointures

---
+++ src/DataFixtures/ActorFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Actor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class ActorFixtures extends Fixture
+{
+    public const ACTORS = [
+        'Kaley Cuoco',
+        'Johnny Galecki',
+        'Jim Parsons',
+        'Kunal Nayyar',
+        'Melissa Rauch',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::ACTORS as $key => $actorName) {
+            $actor = new Actor();
+            $actor->setName($actorName);
+            $manager->persist($actor);
+            $this->addReference('actor_' . $key, $actor);
+        }
+        $manager->flush();
+    }
+}
